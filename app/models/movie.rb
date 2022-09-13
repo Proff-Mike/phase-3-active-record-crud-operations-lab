fef1ba7f1848a1ab820efa422b7000e6ce3f9c80
Michael Okoe <michael.okoejr@gmail.com>
2022-09-13 18:34:40 +0000

    Created methods in movies.rb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,46 @@
 class Movie < ActiveRecord::Base
+  def self.create_with_title(title)
+    create(title: title)
+  end
 
-end
+   def self.first_movie
+     first
+   end
+  
+   def self.last_movie
+     last
+   end
+
+   def self.movie_count
+     count
+   end
+
+   def self.find_movie_with_id(id)
+     find(id)
+   end
+
+   def self.find_movie_with_attributes(attr)
+     find_by(attr)
+   end
+
+   def self.find_movies_after_2002
+     where("release_date > 2002")
+   end
+
+   def update_with_attributes(attr)
+     self.update(attr)
+   end
+
+   def self.update_all_titles(title)
+     update(title: title)
+   end
+
+   def self.delete_by_id(id)
+     find(id).destroy
+   end
+
+   def self.delete_all_movies
+     destroy_all
+   end
+   
+ end
